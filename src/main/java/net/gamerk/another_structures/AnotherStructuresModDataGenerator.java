121cab17a86b75be7f58bbbc1ee22ac727d2c62b
GamerK-2 <71877778+GamerK-2@users.noreply.github.com>
2024-01-07 13:10:36 +0900

    Initial commit

---
+++ src/main/java/net/gamerk/another_structures/AnotherStructuresModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gamerk.another_structures;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AnotherStructuresModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
